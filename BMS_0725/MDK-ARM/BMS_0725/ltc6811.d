482f5d184ebe0ddd4678fb8a7f65f3d7130c6e2c
zhuzeyu <z58185568@163.com>
2026-07-25 16:43:49 +0800

    initial commit

---
+++ BMS_0725/MDK-ARM/BMS_0725/ltc6811.d
@@ -0,0 +1,6 @@
+bms_0725\ltc6811.o: ..\Core\Src\LTC6811.c
+bms_0725\ltc6811.o: E:\SOFTWARE_PACKAGE_INSTALLING\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+bms_0725\ltc6811.o: ../Core/Inc/LTC6811.h
+bms_0725\ltc6811.o: E:\SOFTWARE_PACKAGE_INSTALLING\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+bms_0725\ltc6811.o: ../Core/Inc/LTC681x.h
+bms_0725\ltc6811.o: ../Core/Inc/LT_SPI.h
---
+++ BMS_0725/MDK-ARM/BMS_0725/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+bms_0725\startup_stm32f103xb.o: startup_stm32f103xb.s
